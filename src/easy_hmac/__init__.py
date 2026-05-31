c6d0f6a5a89c2db2b60c73dad85c22fc0f0d198e
Garreth Cain <garrethccain@gmail.com>
2026-05-31 10:28:17 +1000

    refactor: clean up code style, types, docs, and migrate tests to pytest
    - Add return type annotations to generate_hmac_sha256 and verify_hmac
    - Complete verify_hmac docstring with all parameter descriptions
    - Rename camelCase locals to snake_case (content_type, content_md5)
    - Raise exceptions inline instead of pre-constructing
    - Remove stale TODO and inline comments from utils/http.py
    - Re-export public API from __init__.py
    - Migrate tests from unittest to pytest

--- src/easy_hmac/__init__.py
+++ src/easy_hmac/__init__.py
@@ -1,1 +1,11 @@
+from easy_hmac.core import generate_hmac_sha256, verify_hmac
+from easy_hmac.exceptions import AuthenticationFailed
+
 __version__ = "1.2.1"
+
+__all__ = [
+    "generate_hmac_sha256",
+    "verify_hmac",
+    "AuthenticationFailed",
+    "__version__",
+]
--- tests/test_utils_http.py
+++ tests/test_utils_http.py
@@ -1,48 +1,20 @@
-# Test suite for easy_hmac
+import pytest
+
 from easy_hmac.utils.http import parse_http_date
-import unittest
 
 
-class TestUtilsHTTP(unittest.TestCase):
-    """
-    Houses the tests for the easy_hmac.utils.http
-    """
-
-    def test_parse_http_date_wrong_date_format(self):
-        """
-        Tests if a date on a format not accepted by parse_http_date
-        raises a ValueError exception
-        """
-        wrong_date = "Monday, 14/12/2021 - 10:47:23"
-        self.assertRaises(ValueError, parse_http_date, wrong_date)
-
-    def test_parse_http_date_RFC1123(self):
-        """
-        Given a date on the RFC1123 format, tests if it correctly parses to epoch
-        Epoch value, 784111777, was given by https://www.epochconverter.com/
-        """
-        RFC1123 = "Sun, 06 Nov 1994 08:49:37 GMT"
-        actual = parse_http_date(RFC1123)
-        self.assertEqual(784111777, actual)
-
-    def test_parse_http_date_RFC850(self):
-        """
-        Given a date on the RFC850 format, tests if it correctly parses to epoch
-        Epoch value, 784111777, was given by https://www.epochconverter.com/
-        """
-        RFC850_date = "Sunday, 06-Nov-94 08:49:37 GMT"
-        actual = parse_http_date(RFC850_date)
-        self.assertEqual(784111777, actual)
-
-    def test_parse_http_date_ASCII(self):
-        """
-        Given a date on the ASCII format, tests if it correctly parses to epoch
-        Epoch value, 784111777, was given by https://www.epochconverter.com/
-        """
-        ASCII_date = "Sun Nov  6 08:49:37 1994"
-        actual = parse_http_date(ASCII_date)
-        self.assertEqual(784111777, actual)
+def test_parse_http_date_wrong_date_format():
+    with pytest.raises(ValueError):
+        parse_http_date("Monday, 14/12/2021 - 10:47:23")
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_parse_http_date_rfc1123():
+    assert parse_http_date("Sun, 06 Nov 1994 08:49:37 GMT") == 784111777
+
+
+def test_parse_http_date_rfc850():
+    assert parse_http_date("Sunday, 06-Nov-94 08:49:37 GMT") == 784111777
+
+
+def test_parse_http_date_asctime():
+    assert parse_http_date("Sun Nov  6 08:49:37 1994") == 784111777
